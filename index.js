5f8cdcef7d57a616a36230fc78769f7a2f3015d6
abhi2002 <abhishekdhole60@gmail.com>
2024-07-08 15:34:18 +0530

    added documentation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require("path");
 const routers = require("./routers/routers");
 const productRouters = require("./routers/products");
 const UserCart = require("./routers/userCart");
@@ -10,13 +11,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// View engine setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 dotenv.config({ path: "./.env" });
 // mongodb connection
 const connectToDatabase = require("./connection");
+const { render } = require("ejs");
 connectToDatabase();
 
+// app.get("/", (req, res) => {
+//   res.json({ message: "Hello from backend !" });
+// });
+
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from backend !" });
+  res.render("home.ejs");
 });
 
 // routers
--- views/data.js
+++
@@ -1,5 +0,0 @@
-// NrKTM4mD0Ye91N90
-// NrKTM4mD0Ye91N90
-// devloper232
-
-// mongodb+srv://devloper232:<password>@test-db.ehnf8qc.mongodb.net/
